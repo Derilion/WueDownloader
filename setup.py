d9b84380d26e29c2a5da4dd46c494958c4ffef79
derilion <mbier@gmx.net>
2019-10-21 19:18:38 +0200

    Updated links

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="A small script to download current course files",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/derilion/sampleproject",
+    url="https://github.com/Derilion/WueDownloader",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
